a1667837b9d122a78c265ae4be42182050257d56
yg-PC\yg <persnet3@naver.com>
2022-01-08 15:40:50 +0900

    Bv001_C1  aws iot thing의 이름/ End point / 와이파이 아이디 / 와이파이 비밀번호를 configure 관련 변경 관련 Configure를 원래 aws_clientcredential.h 에서 하는데 이렇게하면 app 단에서 수정하기가 불편해서 #include "My_ClientCredential.h 라는 파일을 inlcude 하도록 하고, 그 안에서 YHS_MY_xxx  형태로 Define 하도록했다. 해당 파일은 App 쪽에 추가될거고, 비밀번호와 연관돼있어서 App 쪽에서 git ignore 될 것이다.

--- Middlewares/Third_Party/amazon-freertos/demos/include/aws_clientcredential.h
+++ Middlewares/Third_Party/amazon-freertos/demos/include/aws_clientcredential.h
@@ -26,19 +26,21 @@
 #ifndef __AWS_CLIENTCREDENTIAL__H__
 #define __AWS_CLIENTCREDENTIAL__H__
 
+#include "My_ClientCredential.h"
+
 /*
  * @brief MQTT Broker endpoint.
  *
  * @todo Set this to the fully-qualified DNS name of your MQTT broker.
  */
-#define clientcredentialMQTT_BROKER_ENDPOINT         ""
+#define clientcredentialMQTT_BROKER_ENDPOINT         YHS_MY_ENDPOINT
 
 /*
  * @brief Host name.
  *
  * @todo Set this to the unique name of your IoT Thing.
  */
-#define clientcredentialIOT_THING_NAME               ""
+#define clientcredentialIOT_THING_NAME               YHS_MY_THING
 
 /*
  * @brief Port number the MQTT broker is using.
@@ -55,13 +57,13 @@
  *
  * @todo If you are using Wi-Fi, set this to your network name.
  */
-#define clientcredentialWIFI_SSID                    ""
+#define clientcredentialWIFI_SSID                    YHS_MY_WIFI_SSID
 
 /*
  * @brief Password needed to join Wi-Fi network.
  * @todo If you are using WPA, set this to your network password.
  */
-#define clientcredentialWIFI_PASSWORD                ""
+#define clientcredentialWIFI_PASSWORD                YHS_MY_WIFI_PASSWORD
 
 /*
  * @brief Wi-Fi network security type.
